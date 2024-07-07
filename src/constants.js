1a8a32e5a6e672aecbcdb89ce3c4e9dbcaebe9d2
Ilyas Tahari <114678345+ilyastahari@users.noreply.github.com>
2024-07-07 00:15:01 -0400

    Delete src directory

--- src/constants.js
+++
@@ -1,2 +0,0 @@
-export const SLACK_OAUTH_TOKEN = 'SLACK_OAUTH_TOKEN'
-export const BOT_TEST_CHANNEL = '#bot-test'
--- src/index.js
+++
@@ -1,5 +0,0 @@
-require = require("esm")(module/*, options*/)
-module.exports = require("./app.js")
-
-require('dotenv').config();
-console.log(process.env);
